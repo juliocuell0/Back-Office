90f5326760bd4470e171e341a36deb7732d7f5bf
Gav14 <maurogavilan8@gmail.com>
2026-03-10 15:44:54 -0300

    clase Alumno

---
+++ src/main/java/ar/edu/itec/Model/Alumno.java
@@ -0,0 +1,25 @@
+package ar.edu.itec.Model;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Alumno {
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+    public Alumno(String dni, String nombre, String apellido, String telefono, String email) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.activo = true;
+    }
+}
